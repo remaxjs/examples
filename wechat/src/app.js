9ccc6c009d9ce8e031d9f8f86782df88933a855f
caihuanyu <eterlf41@gmail.com>
2019-08-13 12:36:53 +0800

    更新 todo list

--- wechat/src/app.js
+++ wechat/src/app.js
@@ -4,8 +4,7 @@ class App {
   todos = [
     { id: 1, text: 'Learning Javascript', completed: true },
     { id: 2, text: 'Learning ES2016', completed: true },
-    { id: 3, text: 'Learning 支付宝小程序', completed: false },
-    { id: 4, text: 'Learning 微信小程序', completed: false }
+    { id: 3, text: 'Learning Remax', completed: false }
   ];
 }
 
