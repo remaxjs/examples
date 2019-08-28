2e155ed8283b5d9147b76bf03e302bd2dbabd747
Pochodaydayup <13343460801@163.com>
2019-08-28 18:41:55 +0800

    add toutiao todo demo

---
+++ toutiao/src/app.config.js
@@ -0,0 +1,10 @@
+const backgroundColor = '#323239';
+const pages = ['pages/index/index', 'pages/new/index'];
+
+module.exports = {
+  pages,
+  window: {
+    navigationBarTitleText: 'Toutiao Todo App',
+    navigationBarBackgroundColor: backgroundColor,
+  },
+};
---
+++ toutiao/src/app.js
@@ -0,0 +1,11 @@
+import './app.css';
+
+class App {
+  todos = [
+    { id: 1, text: 'Learning Javascript', completed: true },
+    { id: 2, text: 'Learning ES2016', completed: true },
+    { id: 3, text: 'Learning Remax', completed: false }
+  ];
+}
+
+export default App;
