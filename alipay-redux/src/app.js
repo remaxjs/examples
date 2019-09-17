8a076abc57bdcf5b1188a098a0a932831ff56662
Wei Zhu <yesmeck@gmail.com>
2019-09-17 15:09:49 +0800

    Add redux demo

---
+++ alipay-redux/src/app.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'remax-redux';
+import rootReducer from './reducers';
+import './app.css';
+
+const store = createStore(rootReducer);
+
+const App = ({ children }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+export default App;
