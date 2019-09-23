0bf7db92d8ede43b752d1cb82fc5a7682a1571e9
Wei Zhu <yesmeck@gmail.com>
2019-09-23 16:02:15 +0800

    Add apollo example

---
+++ alipay-apollo/src/app.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import './app.css';
+
+class Response {
+  constructor(body, init) {
+    this.body = body;
+    this.init = init;
+  }
+
+  text() {
+    return Promise.resolve(this.body);
+  }
+}
+
+const client = new ApolloClient({ uri: 'https://etmdb.com/graphql', fetch: (url, options) => {
+  return new Promise((resolve, reject) => {
+    my.request({
+      url,
+      method: options.method,
+      headers: options.headers,
+      data: options.body,
+      success(res) {
+        resolve(new Response(JSON.stringify(res.data)));
+      },
+      fail(error) {
+        resolve(error);
+      }
+    })
+  });
+} });
+
+const App = ({ children }) => {
+  return (
+    <ApolloProvider client={client}>
+      {children}
+    </ApolloProvider>
+  )
+};
+
+export default App;
