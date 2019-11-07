a0eb12445f1f0d3b49a94f0427b7dac186ac2ceb
caihuanyu <eterlf41@gmail.com>
2019-11-07 13:03:01 +0800

    add wechat mobx example

---
+++ wechat-mobx/src/app.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import createTodoStore from '@/stores/TodoStore';
+import { useLocalStore } from 'mobx-remax';
+import { storeContext } from '@/hooks/useStores';
+import './app.css';
+
+const App = ({ children }) => {
+  const todoStore = useLocalStore(createTodoStore);
+  return (
+    <storeContext.Provider value={{ todoStore }}>
+      {children}
+    </storeContext.Provider>
+  );
+};
+
+export default App;
