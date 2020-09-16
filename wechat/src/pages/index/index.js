8d80c164a47c222cf1075abb3c29232b3468be0e
农有宝 <nongyb@weipaitang.com>
2020-09-16 11:04:17 +0800

    Update index.js

--- wechat/src/pages/index/index.js
+++ wechat/src/pages/index/index.js
@@ -1,8 +1,17 @@
 import * as React from 'react';
 import { View, navigateTo, Button } from 'remax/wechat';
+import { usePageEvent } from '@remax/macro';
 import './index.css';
 
 export default () => {
+
+    usePageEvent('onShow', () => {
+        console.log('onSHow', 'index');
+    });
+
+    usePageEvent('onHide', () => {
+        console.log('onHide', 'index');
+    });
   
     return <View className="page-todos">index <Button onClick={()=> {
        navigateTo({ url: '../new/index' });
